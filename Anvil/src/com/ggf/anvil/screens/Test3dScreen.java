257580e5f88240cd7dc5cb3838be6cdb1e73ca36
Fritz Jacob <fjacob@tzi.de>
2014-01-29 23:09:36 +0100

    worked on 3d models

--- Anvil/src/com/ggf/anvil/screens/Test3dScreen.java
+++ Anvil/src/com/ggf/anvil/screens/Test3dScreen.java
@@ -1,5 +1,6 @@
 package com.ggf.anvil.screens;
 
+import com.badlogic.gdx.Files.FileType;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
@@ -10,9 +11,14 @@ import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.loader.G3dModelLoader;
+import com.badlogic.gdx.graphics.g3d.utils.AnimationController;
+import com.badlogic.gdx.graphics.g3d.utils.AnimationController.AnimationDesc;
+import com.badlogic.gdx.graphics.g3d.utils.AnimationController.AnimationListener;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.UBJsonReader;
 import com.ggf.anvil.Anvil;
 
 public class Test3dScreen extends AbstractScreen {
@@ -20,10 +26,10 @@ public class Test3dScreen extends AbstractScreen {
 	public PerspectiveCamera cam;
     public CameraInputController camController;
     public ModelBatch modelBatch;
-    public AssetManager assets;
     public Array<ModelInstance> instances = new Array<ModelInstance>();
     public Environment environment;
-    public boolean loading;
+    
+    public AnimationController aniController;
 
 	public Test3dScreen(Anvil game) {
 		super(game);
@@ -35,7 +41,7 @@ public class Test3dScreen extends AbstractScreen {
         environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
          
         cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        cam.position.set(4f, 4f, 4f);
+        cam.position.set(8f, 4f, 4f);
         cam.lookAt(0,0,0);
         cam.near = 0.1f;
         cam.far = 300f;
@@ -44,32 +50,40 @@ public class Test3dScreen extends AbstractScreen {
         camController = new CameraInputController(cam);
         Gdx.input.setInputProcessor(camController);        
         
-        assets = new AssetManager();
-        assets.load("models/anvil_1.obj", Model.class);
-        loading = true;
-	}
- 
-    private void doneLoading() {
-        Model anvil = assets.get("models/anvil_1.obj", Model.class);
-        ModelInstance anvilInstance = new ModelInstance(anvil); 
+        UBJsonReader jsonReader = new UBJsonReader();
+        G3dModelLoader modelLoader = new G3dModelLoader(jsonReader);
+        Model smith = modelLoader.loadModel(Gdx.files.getFileHandle("models/smith_2_4.g3db", FileType.Internal));
+        Model anvil = modelLoader.loadModel(Gdx.files.getFileHandle("models/anvil_2.g3db", FileType.Internal));
+
+        ModelInstance anvilInstance = new ModelInstance(anvil);
+        anvilInstance.transform.rotate(new Vector3(1, 0, 0), -90f);
+        anvilInstance.transform.rotate(new Vector3(0, 0, 1), 120f);
+        anvilInstance.transform.translate(0, 0, -1.5f);
         instances.add(anvilInstance);
-        loading = false;
-    }
+        
+        ModelInstance smithInstance = new ModelInstance(smith);
+        smithInstance.transform.rotate(new Vector3(1, 0, 0), -90f);
+        smithInstance.transform.rotate(new Vector3(0, 0, 1), 50f);
+        smithInstance.transform.translate(4f, 4f, 0);
+        instances.add(smithInstance);
+        
+        aniController = new AnimationController(smithInstance);  
+        aniController.setAnimation("Idle1", -1);	//TODO: animation does not work
+	}
      
     @Override
     public void render(float delta) {
     	
-        if (loading && assets.update())
-            doneLoading();
-        camController.update();
-        
-        if(instances.size > 0 ) {
-        	ModelInstance anvilInstance = instances.get(0);
-        	anvilInstance.transform.rotate( new Vector3(0,1,0), delta * 10);
-        }
-         
         Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+        
+        camController.update();
+        
+        
+        cam.rotateAround(Vector3.Zero, new Vector3(0,1,0),1f);
+        cam.update();
+        
+        aniController.update(Gdx.graphics.getDeltaTime());
         
         modelBatch.begin(cam);
         modelBatch.render(instances, environment);
@@ -80,6 +94,5 @@ public class Test3dScreen extends AbstractScreen {
     public void dispose() {
         modelBatch.dispose();
         instances.clear();
-        assets.dispose();
     }	
 }
